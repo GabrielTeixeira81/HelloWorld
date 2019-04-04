a5bcd6fd7041a3edd864fc0813ef47cc25d13519
GabrielTeixeira81 <gatecosvh>
2019-04-03 22:45:31 -0300

    atualizacao exercicio4

--- src/br/com/digitalhouse/Exercicio4.java
+++ src/br/com/digitalhouse/Exercicio4.java
@@ -5,11 +5,15 @@ public class Exercicio4 {
     public static void main(String[] args) {
 
         int[] numeros = new int[4];
-        numeros[0] =
-        System.out.println(somaDosElementos());
+        numeros[0] = 1;
+        numeros[1] = 23;
+        numeros[2] = 2;
+        numeros[3] = 12;
 
+        for (int i = 0; i < numeros.length; i++) {
+
+            System.out.println(numeros[i]);
+
+        }
     }
-
-    public static float (somaDosElementos())
-
 }
